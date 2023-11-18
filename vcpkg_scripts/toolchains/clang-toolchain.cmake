fa392c8a2bfc192d1d3817d594610848da1077e7
Elad Maimoni <elad.maimong@ultrawis.com>
2023-11-18 09:45:05 +0200

    added boost

--- vcpkg_scripts/toolchains/clang-toolchain.cmake
+++ vcpkg_scripts/toolchains/clang-toolchain.cmake
@@ -1,17 +1,30 @@
+macro(append_flag_if_not_found var flag)
+    string(FIND "${${var}}" "${flag}" position)
+    if(position EQUAL -1)
+        # The string was not found; append it
+
+        message(STATUS "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ append ${flag} to ${var}")
+        message(STATUS "flags before: ${${var}}")
+        string(APPEND ${var} " ${flag} ")
+        message(STATUS "flags after: ${${var}}")
+    endif()
+endmacro()
+
 
 
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 
-string(APPEND CMAKE_CXX_FLAGS_INIT "-stdlib=libc++")
+append_flag_if_not_found(CMAKE_CXX_FLAGS_INIT "-stdlib=libc++")
+append_flag_if_not_found(CMAKE_CXX_FLAGS_INIT "-fPIC")
 
 set(CMAKE_SYSTEM_PROCESSOR x86_64 CACHE STRING "")
 
 get_property(_CMAKE_IN_TRY_COMPILE GLOBAL PROPERTY IN_TRY_COMPILE)
 
 if(NOT _CMAKE_IN_TRY_COMPILE)
-    string(APPEND CMAKE_C_FLAGS_INIT " -fPIC ${VCPKG_C_FLAGS} ")
-    string(APPEND CMAKE_CXX_FLAGS_INIT " -fPIC ${VCPKG_CXX_FLAGS} ")
+    string(APPEND CMAKE_C_FLAGS_INIT " ${VCPKG_C_FLAGS} ")
+    string(APPEND CMAKE_CXX_FLAGS_INIT " ${VCPKG_CXX_FLAGS} ")
     string(APPEND CMAKE_C_FLAGS_DEBUG_INIT " ${VCPKG_C_FLAGS_DEBUG} ")
     string(APPEND CMAKE_CXX_FLAGS_DEBUG_INIT " ${VCPKG_CXX_FLAGS_DEBUG} ")
     string(APPEND CMAKE_C_FLAGS_RELEASE_INIT " ${VCPKG_C_FLAGS_RELEASE} ")
@@ -37,7 +50,7 @@ endif()
 
 # set(CMAKE_C_FLAGS "-fuse-ld=lld")
 # set(CMAKE_CXX_FLAGS "-fuse-ld=lld")
-set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+# set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
 # set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
